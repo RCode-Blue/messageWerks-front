6f011462eb98500717e2d9ed44085fb3fbca0eac
Ricky <rickyliew@hotmail.com>
2022-03-27 22:49:42 +1100

    implement password masking

--- src/components/landing/login.js
+++ src/components/landing/login.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import PasswordField from "./passwordField";
+// import Form from "react-validation/build/form";
+// import Input from "react-validation/build/input";
 
 const Login = () => {
-  return <div className="">LOGIN FORM</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onEmailChange = (e) => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+  const onPasswordChange = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("email: ", email, "password: ", password);
+  };
+
+  return (
+    <div className="login-form">
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={onEmailChange}
+          placeholder="email *"
+        />
+        <PasswordField
+          onPasswordChange={onPasswordChange}
+          password={password}
+        />
+
+        <input type="submit" value="Log in" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/components/landing/passwordField.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+
+const PasswordField = (props) => {
+  const [fieldType, setFieldType] = useState("password");
+  const handleMaskClick = () => {
+    let newFieldType = fieldType === "text" ? "password" : "text";
+    setFieldType(newFieldType);
+  };
+
+  return (
+    <div>
+      <input
+        type={fieldType}
+        placeholder="password *"
+        onChange={props.onPasswordChange}
+        value={props.password}
+      ></input>
+      <span>
+        <FontAwesomeIcon icon={faEye} onClick={handleMaskClick} />
+      </span>
+    </div>
+  );
+};
+export default PasswordField;
